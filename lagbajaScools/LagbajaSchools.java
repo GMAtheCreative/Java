6e0606b31f0b0b17df6fb6b5b69750c1947e1674
GMAtheCreative <timothywonder01@gmail.com>
2024-08-04 10:13:27 +0100

    undone

---
+++ lagbajaScools/LagbajaSchools.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class LagbajaSchools{
+	public static void main(String... args){
+
+		Scanner collect = new Scanner(System.in);
+
+		System.out.println("How many student do you have?");
+		int numberOfStudent = collect.nextInt();
+
+		System.out.println("How many subject do they offer?");
+		int subjectTheyOffer = collect.nextInt();
+
+		System.out.println("Saving>>>>>>>>>>>>>>>>>>>>>>>>>...");
+		System.out.println("Successfully saved");
+		System.out.println("===================================\n");
+		
+		int [][]gradeCollection = new int[numberOfStudent][subjectTheyOffer];
+
+		for(int studentCounter =0 ; studentCounter < numberOfStudent ; studentCounter++){
+			System.out.println("Enter scores of student " + (studentCounter+1));
+
+			for (int scoreCounter = 0; scoreCounter < subjectTheyOffer; scoreCounter++){
+
+				gradeCollection[studentCounter][scoreCounter] = collect.nextInt();
+			}
+			System.out.println();
+		}
+
+
+		
+	}
+}
